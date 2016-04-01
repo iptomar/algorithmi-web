4b52f333fc8a817ba3c72727527dfa775b66d8b5
CristianaPereira <Cristiana_67@hotmail.com>
2016-04-01 20:59:46 +0100

    First commit

--- src/main/java/algorithmi/Main.java
+++ src/main/java/algorithmi/Main.java
@@ -15,9 +15,12 @@
  */
 package algorithmi;
 
-import jdk.nashorn.internal.ir.debug.JSONWriter;
-import jdk.nashorn.internal.parser.JSONParser;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static spark.Spark.delete;
+import static spark.Spark.externalStaticFileLocation;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
@@ -29,58 +32,61 @@ import static spark.Spark.staticFileLocation;
  */
 public class Main {
 
-  public static void main(String[] args) {
-    staticFileLocation("/public");
+    public static void main(String[] args) {
+        try {
+            //staticFileLocation("/public");
+            externalStaticFileLocation(URLDecoder.decode(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath(), "UTF-8") + "/../../src/main/resources/public");
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-    get("/quiz/:id", (request, response) -> {
-      return "Hello World";
-    });
+        get("/quiz/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    post("/quiz", (request, response) -> {
-      return "Hello World";
-    });
+        post("/quiz", (request, response) -> {
+            return "Hello World";
+        });
 
-    put("/quiz/:id", (request, response) -> {
-      return "Hello World";
-    });
+        put("/quiz/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    delete("/quiz/:id", (request, response) -> {
-      return "Hello World";
-    });
-    
-    
-    get("/question/:id", (request, response) -> {
-      return "Hello World";
-    });
+        delete("/quiz/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    post("/question", (request, response) -> {
-      return "Hello World";
-    });
+        get("/question/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    put("/question/:id", (request, response) -> {
-      return "Hello World";
-    });
+        post("/question", (request, response) -> {
+            return "Hello World";
+        });
 
-    delete("/question/:id", (request, response) -> {
-      return "Hello World";
-    });
+        put("/question/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    
-    get("/user/:id", (request, response) -> {
-      return "{\"username\": \"johndoe\", \"escola\":\"xpto\"}";
-    });
-    
-    post("/user", (request, response) -> {
-      return "Hello World";
-    });
+        delete("/question/:id", (request, response) -> {
+            return "Hello World";
+        });
 
-    put("/user/:id", (request, response) -> {
-      return "Hello World";
-    });
+        get("/user/:id", (request, response) -> {
+            return "{\"username\": \"johndoe\", \"escola\":\"xpto\"}";
+        });
 
-    delete("/user/:id", (request, response) -> {
-      return "Hello World";
-    });
+        post("/user", (request, response) -> {
+            return "Hello World";
+        });
 
-  }
+        put("/user/:id", (request, response) -> {
+            return "Hello World";
+        });
+
+        delete("/user/:id", (request, response) -> {
+            return "Hello World";
+        });
+
+    }
 }
