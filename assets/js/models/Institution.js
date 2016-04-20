c7b007759e5275533313ef79e82fbcd207a72111
Fábio <fabio.arcruz@gmail.com>
2016-04-20 04:12:24 +0100

    INSTITUTIONS VIEW MELHORADA

---
+++ assets/js/models/Institution.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Fábio Cruz on 11/04/2016.
+ */
+var Course = Backbone.Model.extend({
+    urlRoot: '/institution/new'
+});
--- assets/js/views/institutions.js
+++ assets/js/views/institutions.js
@@ -2,7 +2,23 @@
  * Created by Fábio Cruz on 10/04/2016.
  */
 window.InstitutionsView = Backbone.View.extend({
-    events: {},
+    events: {
+        "submit": "beforeSend"
+    },
+
+    beforeSend: function (e) {
+        e.preventDefault();
+
+        modem('POST', 'institution/new',
+            function (json) {
+            },
+            function (xhr, ajaxOptions, thrownError) {
+            },
+            $("#newInstituicao").serializeArray()
+        );
+
+    },
+
     initialize: function () {
     },
 
