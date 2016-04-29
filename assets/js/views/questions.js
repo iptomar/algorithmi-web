19f0ab13e41157bdc92465c47b98f102f0cee62e
Fábio <fabio.arcruz@gmail.com>
2016-04-29 04:32:04 +0100

    POP UP'S DE QUESTIONS, TESTS,INSTITUTIONS,SCHOOLS E COURSES

--- assets/js/views/questions.js
+++ assets/js/views/questions.js
@@ -1,13 +1,18 @@
 window.QuestionsView = Backbone.View.extend({
 
     events: {
-
         "submit": "beforeSend",
-       
+        "click #newQuestion": "newQuestionPopup",
+        "submit #newPopUpQuestion": "newQuestion",
+        "click #addLanguage": "adicionarLinguagem",
+        "click #removeLanguage":"removerLinguagem",
+
     },
 
     beforeSend: function (e) {
         e.preventDefault();
+
+        //this.checkAuth();
 
         modem('POST', 'question/new',
             function (json) {
@@ -19,25 +24,108 @@ window.QuestionsView = Backbone.View.extend({
 
     },
 
+    newQuestion: function(){
 
+        modem('POST', 'popupquestion',
+            function (json) {
+                $("#newQuestionModal").modal('hide');
+            },
+            function (xhr, ajaxOptions, thrownError) {
+                //Mandar Uma Mensgame Qualquer
+            },
+            encodeURI(JSON.stringify($("#newPopUpQuestion").serializeObject()))
+        );
 
-    adicionarLinguagem: function (){
-        var $html = $('<p>ola</p>');
-        var str = $html.prop('outerHTML');
-        console.log(str);
-        $("#language").append($html);
+    },
+
+    newQuestionPopup: function(e){
+        e.preventDefault();
+
+        $("#newQuestionModal").modal('show');
 
     },
 
 
 
-
+    checkAuth: function(){
+        if(!sessionStorage.getItem('keyo')){
+            showLoginModal($("#someParent"));
+        }
+    },
 
     initialize: function () {
     },
 
     render: function () {
         $(this.el).html(this.template());
+
+        showLoginModal($("#someParent"));
+
         return this;
-    }
+    },
+
+
+    adicionarLinguagem: function (){
+
+        var $language = $('#linguagens').find(":selected").text();
+        console.log($language);
+
+        switch($language){
+            case "JAVA":
+                $("#language")
+                    .append($("<div>", {class: "col-md-2"})
+                        .append($("<img>", {
+                            src: "assets/ico/JAVA.png",
+                            alt: "Smiley Face",
+                            width: "37",
+                            height: "37"
+                        }))
+                    );
+                break;
+            case "PHP":
+                $("#language")
+                    .append($("<div>", {class: "col-md-2"})
+                        .append($("<img>", {
+                            src: "assets/ico/PHP.png",
+                            alt: "Smiley Face",
+                            width: "37",
+                            height: "37"
+                        }))
+                    );
+                break;
+            case "C":
+                $("#language")
+                    .append($("<div>", {class: "col-md-2"})
+                        .append($("<img>", {
+                            src: "assets/ico/c.png",
+                            alt: "Smiley Face",
+                            width: "37",
+                            height: "37"
+                        }))
+                    );
+                break;
+            case "C++":
+                $("#language")
+                    .append($("<div>", {class: "col-md-2"})
+                        .append($("<img>", {
+                            src: "assets/ico/c++.png",
+                            alt: "Smiley Face",
+                            width: "37",
+                            height: "37"
+                        }))
+                    );
+                break;
+            default:
+                break;
+        }
+
+
+
+
+
+
+
+    },
 });
+
+
