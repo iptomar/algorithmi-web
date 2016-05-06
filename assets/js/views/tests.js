0ccfa1cef944c122368ead68b7106586a1f453b2
Fábio <fabio.arcruz@gmail.com>
2016-05-06 10:37:25 +0100

    Função de Remover Pergunta em Teste a funcionar e UserName addicionado ao SignUp

--- assets/js/views/tests.js
+++ assets/js/views/tests.js
@@ -2,7 +2,7 @@ window.TestsView = Backbone.View.extend({
 
 
     events: {
-        "click #addLine": "adicionarPergunta",
+        "click #addLine": "adicionarPergunta", ////O Trigger é o Botão novo
         "click #Criar": "beforeSend",
         "click #removeLine": "removerPergunta",
         "click #newTest": "newTestPopup",
@@ -38,7 +38,7 @@ window.TestsView = Backbone.View.extend({
 
     },
 
-
+//Vai abrir Popup para criar novo teste
     newTestPopup: function(e){
         e.preventDefault();
 
@@ -46,13 +46,11 @@ window.TestsView = Backbone.View.extend({
 
     },
 
-
-    checkAuth: function(){
-        if(!sessionStorage.getItem('keyo')){
-            showLoginModal($("#someParent"));
-        }
-    },
-
+    //O Trigger é o Botão novo , vai criar uma linha com
+    //-Tema     -Nivel      -Qnt
+    //-Dados    -Facil      -1
+    //-Decisão  -Médio      -2
+    //-..        -..         -..
     adicionarPergunta: function (){
         var $html = $('<div id="line" class="col-md-12 col-xs-12"><br><div class="col-md-4 col-xs-4"> <div class="input-group"> <span class="input-group-addon btn-white"><i class="fa fa-book"></i></span> <select id="ddCategoria" name="category" class="form-control mandatory"> <option disabled selected>Tema</option> <option value="1">Cálculo Computacional</option> <option value="2">Decisão</option> <option value="3">Iteração</option> <option value="4">Arrays</option> <option value="5">Funções</option> </select> </div> </div> <div class="col-md-4 col-xs-4"> <div class="input-group"> <span class="input-group-addon btn-white"><i class="fa fa-book"></i></span> <select id="ddDificuldade" name="difficulty" class="form-control mandatory"> <option disabled selected>Nivel</option> <option value="1">Fácil</option> <option value="2">Médio</option> <option value="3">Dificil</option> </select> </div> </div> <div class="col-md-4 col-xs-4"> <div class="input-group"> <span class="input-group-addon btn-white"><i class="fa fa-book"></i></span> <select id="dCategoria" name="number" class="form-control mandatory"> <option disabled selected>Qnt</option> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> </select> </div> </div></div> ');
         var str = $html.prop('outerHTML');
@@ -61,8 +59,9 @@ window.TestsView = Backbone.View.extend({
 
     },
 
+    //O Trigger é o Botão Rem , vai remover a ultima linha criada pelo adicionar pergunta
     removerPergunta: function (){
-        $("#line").remove();
+        $("#line:last-child").remove();
     },
 
 
